12df8330041da4be222ff30e4cba07fbd54eec3c
spokeDev <brian@bnkuhn.com>
2015-10-29 07:32:31 -0700

    added citations

--- AngularDemo/AngularJSWebApi/Repository/CitationRepository.cs
+++ AngularDemo/AngularJSWebApi/Repository/CitationRepository.cs
@@ -19,11 +19,11 @@ namespace AngularJSWebApi.Repository
                     citations = new List<Citation>()
                     {
                         new Citation() {ID = 1, CitationNumber = "ABCD1234", Driver = new Person() {LastName = "Smith", FirstName = "Joe" }, ViolationDateTime = DateTime.Now, Fine=124.54m },
-                        new Citation() {ID = 2, CitationNumber = "REDSF1234", Driver = new Person() {LastName = "Jones", FirstName = "Kim" }, ViolationDateTime = DateTime.Now, Fine=124.54m },
-                        new Citation() {ID = 3, CitationNumber = "FTRR1234", Driver = new Person() {LastName = "Smith", FirstName = "Paul" }, ViolationDateTime = DateTime.Now, Fine=124.54m },
-                        new Citation() {ID = 4, CitationNumber = "OIUJR1234", Driver = new Person() {LastName = "Smith", FirstName = "Bob" }, ViolationDateTime = DateTime.Now, Fine=124.54m },
-                        new Citation() {ID = 5, CitationNumber = "QADD1234", Driver = new Person() {LastName = "Tucker", FirstName = "Jeff" }, ViolationDateTime = DateTime.Now, Fine=124.54m },
-                        new Citation() {ID = 6, CitationNumber = "ACDFL1234", Driver = new Person() {LastName = "Barr", FirstName = "Brandt" }, ViolationDateTime = DateTime.Now, Fine=124.54m }
+                        new Citation() {ID = 2, CitationNumber = "REDSF1234", Driver = new Person() {LastName = "Jones", FirstName = "Kim" }, ViolationDateTime = DateTime.Now, Fine=50.34m },
+                        new Citation() {ID = 3, CitationNumber = "FTRR1234", Driver = new Person() {LastName = "Smith", FirstName = "Paul" }, ViolationDateTime = DateTime.Now, Fine=98.00m },
+                        new Citation() {ID = 4, CitationNumber = "OIUJR1234", Driver = new Person() {LastName = "Smith", FirstName = "Bob" }, ViolationDateTime = DateTime.Now, Fine=34.00m },
+                        new Citation() {ID = 5, CitationNumber = "QADD1234", Driver = new Person() {LastName = "Tucker", FirstName = "Jeff" }, ViolationDateTime = DateTime.Now, Fine=100.90m },
+                        new Citation() {ID = 6, CitationNumber = "ACDFL1234", Driver = new Person() {LastName = "Barr", FirstName = "Brandt" }, ViolationDateTime = DateTime.Now, Fine=50.30m }
                     };
                 }
                 return citations;
